28ef37072c8013ac58c118c893a4b616a87c45f9
João Victor <joaoparronchi04@gmail.com>
2023-05-24 21:53:23 -0300

    functions metade

---
+++ funções/tipos.js
@@ -0,0 +1,15 @@
+// 1 - functions declarations junto com o uso de arguments
+function declarations(){
+    const soma = 1 + 4
+    if(soma === 6){
+        return true
+    }
+
+    return false
+}
+console.log(declarations(1, 7))
+
+// 2 - functions expression junto com o uso de arguments
+const expression = function(){
+
+}
