de11ad49e9b0a8ad02b9358c9d1fb22f6cf809a6
João Victor <joaoparronchi04@gmail.com>
2023-05-28 15:15:30 -0300

    função auto invocada

---
+++ funções/IIFE.js
@@ -0,0 +1,7 @@
+//IIFE = Expressão de função invodaca imediatamente
+//é comouma função privada
+(function(idade1,idade2){
+    idade = idade1 + idade2
+    console.log("Sua idade é",idade)
+
+})(2, 8)
